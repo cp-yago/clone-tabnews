f6a0d018a1271767a334c20f683aa31a2a3fd5b7
Yago Cunha <yago.cunha123@gmail.com>
2025-01-21 19:01:15 +0000

    fix  endpoint connection bug

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -15,3 +15,23 @@ test("GET to /api/v1/migrations should return 200", async () => {
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
 });
+
+test("Not allowed methods to /api/v1/migrations should return 405", async () => {
+  const deleteResponse = await fetch(
+    "http://localhost:3000/api/v1/migrations",
+    {
+      method: "DELETE",
+    },
+  );
+  expect(deleteResponse.status).toBe(405);
+
+  const putResponse = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "PUT",
+  });
+  expect(putResponse.status).toBe(405);
+
+  const patchResponse = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "PATCH",
+  });
+  expect(patchResponse.status).toBe(405);
+});
